dd5ff86b46e3891a1975a10969544e93e0334ffd
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-04-14 07:41:06 -0500

    describe unit test

--- src/__tests__/Home/index.test.js
+++ src/__tests__/Home/index.test.js
@@ -23,7 +23,7 @@ describe("<Home />", () => {
   }
 
   global.localStorage = localStorage;
-  global.location.reload = sinon.spy();
+  global.location.reload = sinon.spy(); // is the same that jest.fn()
 
   beforeEach(() => {
     wrapper = mount(<Home {...props} />)
@@ -51,7 +51,7 @@ describe("<Home />", () => {
 
     expect(wrapper.state().githubToken).toEqual(null) // can't be .toBeUndefined(); like tutorial, because localStorage return null
 
-    // console.log(localStorage.getItem.firstCall)
+    console.log(localStorage.getItem.firstCall)
 
     // expect(localStorage.getItem.getCall(0)).toBeDefined(); //.getCall(0) to accest to first call
     // expect(localStorage.getItem.getCall(0).lastArg).toEqual(GITHUB_TOKEN);
@@ -62,19 +62,19 @@ describe("<Home />", () => {
   })
 
   it("call getProfileRepos when reposUrl Exists", () => {
-    expect(wrapper.instance().props.getProfileRepos.getCall(0)).toBeFalsy();
-    wrapper.setProps({ githubData: { repos_url: "something.com.mx" } });
+    expect(wrapper.instance().props.getProfileRepos.getCall(0)).toBeFalsy(); // toBeDefined() is correct, because null is defined
+    wrapper.setProps({ githubData: { repos_url: "something.com.co" } }); // can use too setState
     expect(wrapper.instance().props.getProfileRepos.getCall(0)).toBeDefined();
     expect(wrapper.instance().props.getProfileRepos.getCall(0).lastArg).toBeDefined();
   });
 
   it("Clear Storage and refresh site", () => {
     expect(location.reload.getCall(0)).toBeFalsy();
-    //expect(localStorage.clear.getCall(0)).toBeFalsy();
+    expect(localStorage.clear.getCall(0)).toBeFalsy();
 
     wrapper.instance().handleLogoutClick();
 
-    // expect(localStorage.clear.getCall(0)).toBeTruthy();
+    expect(localStorage.clear.getCall(0)).toBeTruthy();
     // expect(localStorage.clear.getCall(0).lastArg).toBeUndefined();
 
     expect(location.reload.getCall(0)).toBeTruthy();
