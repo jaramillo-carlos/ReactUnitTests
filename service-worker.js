7bc82a03dc7be6bb0da7af139cce55703a9fe9c4
jaramillo-carlos <jaramillo-carlos>
2021-02-17 03:08:28 +0000

    deploy build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.12fa8e81312d4f64db7f91e5a96e724a.js"
+  "https://jaramillo-carlos.github.io/react-unit-test-github-api/precache-manifest.a8e86ac13f347b6d3a51679663d1809b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://jaramillo-carlos.github.io/react-unit-test-github-api/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
