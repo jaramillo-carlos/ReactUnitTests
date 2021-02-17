e22e591dc3d8f524b2cd7cd1ffc4ad3a5da2b6d3
jaramillo-carlos <jaramillo-carlos>
2021-02-17 16:06:56 +0000

    deploy build

---
+++ precache-manifest.79b561b52f2f7aa4ee882cd441802d8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbaac8d687c2f883c7315f22f676f545",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3a3ff8b28ab65de1eb14",
+    "url": "./static/css/main.c3ca6c37.chunk.css"
+  },
+  {
+    "revision": "2655f61e8447bedd3424",
+    "url": "./static/js/2.59d0cdaf.chunk.js"
+  },
+  {
+    "revision": "73556b140791e7f1e95a61af7da7dddb",
+    "url": "./static/js/2.59d0cdaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3ff8b28ab65de1eb14",
+    "url": "./static/js/main.0043e28d.chunk.js"
+  },
+  {
+    "revision": "46ac0d2ec3584ecb670d",
+    "url": "./static/js/runtime-main.ebd294bb.js"
+  }
+]);
