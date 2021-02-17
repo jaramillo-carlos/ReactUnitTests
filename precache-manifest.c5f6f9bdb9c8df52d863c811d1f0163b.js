ecd4bfb5de70925bcb318f645332464ac95e8b2d
jaramillo-carlos <jaramillo-carlos>
2021-02-17 12:44:06 +0000

    deploy build

---
+++ precache-manifest.c5f6f9bdb9c8df52d863c811d1f0163b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26f82dbaa791de9e51e4b6feb330683",
+    "url": "./index.html"
+  },
+  {
+    "revision": "17d36eb41afce563e137",
+    "url": "./static/css/main.c3ca6c37.chunk.css"
+  },
+  {
+    "revision": "2655f61e8447bedd3424",
+    "url": "./static/js/2.59d0cdaf.chunk.js"
+  },
+  {
+    "revision": "73556b140791e7f1e95a61af7da7dddb",
+    "url": "./static/js/2.59d0cdaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17d36eb41afce563e137",
+    "url": "./static/js/main.cfe8d202.chunk.js"
+  },
+  {
+    "revision": "46ac0d2ec3584ecb670d",
+    "url": "./static/js/runtime-main.ebd294bb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-unit-test-github-api/precache-manifest.ecbae34a36af1f909f329998e5869ca9.js"
+  "./precache-manifest.c5f6f9bdb9c8df52d863c811d1f0163b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-unit-test-github-api/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
